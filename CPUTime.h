dc573c73fbc89a1fd726f4bd7e6faffc7f3b1af8
Duje Senta <dsenta00@fesb.hr>
2015-04-08 19:48:14 +0100

    Update CPUTime.h

--- CPUTime.h
+++ CPUTime.h
@@ -2,6 +2,7 @@
 #define CPUTIME_H
 #include "BoxInfo.h"
 
+#if defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
 class CPUTime {
 public:
 	CPUTime()
@@ -16,15 +17,58 @@ public:
 
 	inline void Stop()
 	{
-        	stop = clock();
+		stop = clock();
 	}
 
 	double GetCPUTime()
 	{
-        	return (double)(stop - start)/CLOCKS_PER_SEC;
+		return (double)(stop - start) * 1000 / CLOCKS_PER_SEC;
+	}
+
+	double GetCPU()
+	{
+		return stop-start;
 	}
 private:
     double start;
     double stop;
 };
+#else
+#include <iomanip>
+
+class CPUTime {
+public:
+    CPUTime()
+    {
+        QueryPerformanceFrequency(&freq);
+        secs = 0;
+    }
+
+    inline void Start()
+    {
+        QueryPerformanceCounter(&t1);
+    }
+
+    inline void Stop()
+    {
+        QueryPerformanceCounter(&t2);
+        secs = (double)(t2.QuadPart - t1.QuadPart) * 1000 / (double)freq.QuadPart;
+    }
+
+    inline double GetCPU()
+    {
+        return (double)(t2.QuadPart - t1.QuadPart);
+    }
+
+    double GetCPUTime()
+    {
+        return secs;
+    }
+private:
+    LARGE_INTEGER t1;
+    LARGE_INTEGER t2;
+    LARGE_INTEGER freq;
+    double secs;
+};
+#endif
 #endif
