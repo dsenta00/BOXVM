0699c42ca9f87694405a1cc4d75264e02e01ac9e
Duje Senta <dsenta00@fesb.hr>
2015-05-01 20:30:53 +0200

    Update ByteCode.h

--- ByteCode.h
+++ ByteCode.h
@@ -5,12 +5,12 @@
 
 class ByteCode {
 public:
-    ByteCode();
-    ByteCode(Bcode, Bcode);
-    ~ByteCode();
-    Bcode ReadByteCode();
-    void SetByteCode(Bcode);
-    void Jump(Byte);
+	ByteCode();
+	ByteCode(Bcode, Bcode);
+	~ByteCode();
+	Bcode ReadByteCode();
+	void SetByteCode(Bcode);
+	void Jump(Byte);
 protected:
 	Bcode bytecode;
 	Bcode codesegment;
@@ -20,24 +20,24 @@ ByteCode::~ByteCode()
 {
 	if (codesegment)
 		free(codesegment);
-    bytecode = codesegment = null;
+	bytecode = codesegment = null;
 }
 
 ByteCode::ByteCode()
 {
-    bytecode = null;
-    codesegment = null;
+	bytecode = null;
+	codesegment = null;
 }
 
 ByteCode::ByteCode(Bcode _codesegment, Bcode _bytecode)
 {
-    bytecode = _bytecode;
+	bytecode = _bytecode;
 	codesegment = _codesegment;
 }
 
 void ByteCode::Jump(Byte n)
 {
-    bytecode += n;
+	bytecode += n;
 }
 
 Bcode ByteCode::ReadByteCode()
