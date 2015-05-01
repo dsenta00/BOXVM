b5b635bf2822b8259898fb849b258a68516f9f72
Duje Senta <dsenta00@fesb.hr>
2015-05-01 20:46:21 +0200

    Update Loop.h

--- Loop.h
+++ Loop.h
@@ -7,61 +7,61 @@
 class Loop {
 public:
 	Loop();
-    void PushLoop(Bcode);
-    void PopLoop();
-    Bcode GetStartLoop();
-    void SetMonitor(ProgramMonitor *);
+	void PushLoop(Bcode);
+	void PopLoop();
+	Bcode GetStartLoop();
+	void SetMonitor(ProgramMonitor *);
 protected:
-    Bcode buffer[MAXLOOPSIZE];
-    Bcode *curr;
-    Bcode *make;
-    ProgramMonitor *monitor;
+	Bcode buffer[MAXLOOPSIZE];
+	Bcode *curr;
+	Bcode *make;
+	ProgramMonitor *monitor;
 };
 
 Loop::Loop()
 {
-    curr = STARTLOOP;
-    make = STARTLOOP;
-    memset(buffer, 0, sizeof(Bcode));
+	curr = STARTLOOP;
+	make = STARTLOOP;
+	memset(buffer, 0, sizeof(Bcode));
 }
 
 void Loop::SetMonitor(ProgramMonitor *_monitor)
 {
-    monitor = _monitor;
+	monitor = _monitor;
 }
 
 void Loop::PushLoop(Bcode _adress)
 {
-    if (make != ENDLOOP)
-    {
-        *make = _adress;
-        curr = make;
-        make++;
-    }
-    else
-    {
-        SETERR(LOOP_OVERFLOW);
-    }
+	if (make != ENDLOOP)
+	{
+		*make = _adress;
+		curr = make;
+		make++;
+	}
+	else
+	{
+		SETERR(LOOP_OVERFLOW);
+	}
 }
 
 void Loop::PopLoop()
 {
-    if (make != STARTLOOP)
-    {
-        make = curr;
+	if (make != STARTLOOP)
+	{
+		make = curr;
 
-        if (curr != STARTLOOP)
-            make--;
-    }
+		if (curr != STARTLOOP)
+			make--;
+	}
 	else
-    {
-        SETERR(LOOP_OUT_ERR);
-    }
+	{
+		SETERR(LOOP_OUT_ERR);
+	}
 }
 
 Bcode Loop::GetStartLoop()
 {
-    return *curr;
+	return *curr;
 }
 
 #endif
